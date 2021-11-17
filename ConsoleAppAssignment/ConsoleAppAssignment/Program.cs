4d8ee51e8a4841594def583e13cc88b773c78ece
konadan808 <88794280+konadan808@users.noreply.github.com>
2021-11-17 13:53:37 -0800

    Commit Console App Assignment
    Committing Documentation created for step 229 of the C# course for the Console App Assignment where I had to employ the use of  Boolean comparisons in the use of a while loop and a do while loop.

---
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // App intro text is printed.  then the iteration variable is set as is a new, empty list
+            Console.WriteLine("Your Top Ten Favorite Video Games!!!");
+            int i = 1;
+            var gameList = new List<string>();
+            
+            // The do while code in this block prompts the user to enter their favorite video game titles and adds them to the emptly list until 10 entries are entered
+            do
+            {
+                Console.WriteLine("Entry " + i + ": Please enter a video game title.");
+                gameList.Add(Console.ReadLine());
+                i++;
+            }
+            while (i <= 10);
+
+            // Here the iteration value "l" is set to 0 and the code will print out the list of games that the user entered until all items in the list have been printed.
+            int l = 0;
+            Console.WriteLine("The list of games you have entered are:");
+            while (l < gameList.Count)
+            
+            {
+                Console.WriteLine(gameList[l]);
+                l++;
+            }
+            //Allows the console to stay open and display all the data without closing on its own.
+            Console.ReadLine();
+
+        }
+    }
+}
