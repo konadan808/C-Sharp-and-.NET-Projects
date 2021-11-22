ed6bbeb470e94c66a23b262dfdb7009025e3dd32
konadan808 <88794280+konadan808@users.noreply.github.com>
2021-11-22 13:14:12 -0800

    Commit of Void Method Class Assignment
    Committing the app created per step 281 in the C# course.  This app demonstrates the use of a void method in a class that has user provided input passed as parameters to perform methods on them and then printed to the screen

---
+++ VoidMethodClassAssignment/VoidMethodClassAssignment/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodClassAssignment
+{
+    public class Math
+    {
+        // This method takes in the parameters entered by the user, in which the first number has a math operation done to it and the second number is simply displayed.  Nothing is returned, instead the method is called by the main method.
+        public static void AddAndPrint(int x, int y)
+        {
+            int num1 = x;
+            int sum = x + 5;
+            int num2 = y;
+            Console.WriteLine(x + " plus 5 equals " + sum);
+            Console.WriteLine("The second number you entered is the number " + num2);
+        }
+    }
+}
---
+++ VoidMethodClassAssignment/VoidMethodClassAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This code prompts the user to enter two numbers, one at a time with the expected outcome stated to the user.  User input is converted to int data type.
+            Console.WriteLine("Please enter a number to add by 5:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Plese entere a second number to display to the screen:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            
+            // In this code, the class "Math" metthod of "AddAndPrint" is called with the user input passed along as parameters and the results are printed to the screen
+            Math.AddAndPrint(num1, num2);
+
+            Console.ReadLine();
+        }
+    }
+}
