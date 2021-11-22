0754a0d15fb4e8cb3d22b2a123b68258ebf55685
konadan808 <88794280+konadan808@users.noreply.github.com>
2021-11-22 11:25:34 -0800

    Commit of Main Method Assignment
    Committing the documentation for the Main Method Assignment on step 279 of the C# course.

---
+++ MainMethodAssignment/MainMethodAssignment/Math.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class Math
+    {
+        // This method takes in the user int and the to multiply by int and performas a multiplication operation, returning the result as in int.
+        public static int Multiply(int x, int y)
+        {
+            int num1 = x;
+            int num2 = y;
+            int result = num1 * num2;
+            return result;
+        }
+        // This method takes in the user double and the to multiply by int and perofrms a multiplication operation, returning the result as a double
+        public static double Multiply(double x, double y)
+        {
+            double num1 = x;
+            double num2 = y;
+            double result = num1 * num2;
+            return result;
+        }
+        // This method takes in the user input in it's original data type, converts it to an int data type, adds it to the add to int, and returns the result
+        public static int Multiply(string x, int y)
+        {
+            int num3 = Convert.ToInt32(x);
+            int num4 = y;
+            int result1 = num3 + num4;
+            return result1;
+        }
+        
+    }
+
+    
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This block prompts user to enter a whole number to be multiplied by 5, puts the input into int form, calls the class method to multiply, then prints out the result
+            Console.WriteLine("Please enter a whole number you'd like to see multiplie by 5.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            int result = Math.Multiply(userNum, 5);
+            Console.WriteLine(userNum + " multiplied by 5 equals " + result);
+
+            // This block prompts user to enter a decimal number to be multiplied by 3, puts the input into double form, calls the class method to multiply, then prints out the result
+            Console.WriteLine("Now please enter a decimal number that you'd like to see multiplied by 3.");
+            double userNum2 = Convert.ToDouble(Console.ReadLine());
+            double result1 = Math.Multiply(userNum2, 3);
+            Console.WriteLine(userNum2 + " multiplied by 3 equals " + result1);
+
+            // This block prompts user to enter a number to be added by 8, calls the class method to multiply which takes the user string input it and converts it within the class method to an int, then prints out the result
+            Console.WriteLine("And now please enter a whole number you'd like to see added to by 8");
+            string userNum3 = Console.ReadLine();
+            int result3 = Math.Multiply(userNum3, 8);
+            Console.WriteLine(userNum3 + " plus 8 equals " + result3);
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
