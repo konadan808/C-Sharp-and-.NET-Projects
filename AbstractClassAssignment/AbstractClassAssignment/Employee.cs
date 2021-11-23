8ca81f797e6063977242bb96675ddcdf6d693288
konadan808 <88794280+konadan808@users.noreply.github.com>
2021-11-23 13:14:09 -0800

    Commit of Abstract Class Assignment
    Committing app creation per step 302 of the C# course.  This app demonstrates the use of abstract classes abstract method, and in inheritance

---
+++ AbstractClassAssignment/AbstractClassAssignment/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+
+    // The "Employee" class inherits properties and method from "Person" class
+    public class Employee : Person
+    {
+        // This overrides the original method and allows for new code to take be written, adding specified funtionality to the method
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Demonstrated polymorphism in the creation of this object, just to ensure I undstood how to successfully impliment it.  Then values are added to the objects' properties
+            Person employee1 = new Employee();
+            employee1.FirstName = "Sample";
+            employee1.LastName = "Student";
+
+            // This calls the method first placed in the abstract class "Person" and then inherrited and defined in the "Employee" class that prints out the object's name properties
+            employee1.SayName();
+
+
+            Console.ReadLine();
+        }
+    }
+}
