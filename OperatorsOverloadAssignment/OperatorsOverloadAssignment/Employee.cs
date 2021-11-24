e104be8b95c5bc19fb99124238c29f639cf99802
konadan808 <88794280+konadan808@users.noreply.github.com>
2021-11-24 12:24:36 -0800

    Commit of Operator Overload Assignment
    Committing app created per step 313 of the C# course.  This app demonstrated the overloading of the "==" operator.

---
+++ OperatorsOverloadAssignment/OperatorsOverloadAssignment/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsOverloadAssignment
+{
+    // The "Employee" class inherits properties and method from "Person" class
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+        // This overrides the original method and allows for new code to take be written, adding specified funtionality to the method
+        public override void SayName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+
+        //  This overloads the "==" operator, in which only the object name is needed as a paramter, as the "Id" property is defined in this code as what is to be compared for the objects passed in
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if (employee1.Id.Equals(employee2.Id))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            if (!employee1.Id.Equals(employee2.Id))
+            {
+                return true;
+            }
+            return false;
+        }
+        
+        
+    }
+}
---
+++ OperatorsOverloadAssignment/OperatorsOverloadAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsOverloadAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This code instantiates objects and gives their properties values
+            Employee employee1 = new Employee() { FirstName = "Luke", LastName = "Skywalker", Id = 82 };
+            Employee employee2 = new Employee() { FirstName = "Han", LastName = "Solo", Id = 42 };
+            Employee employee3 = new Employee() { FirstName = "Anikin", LastName = "Skywalker", Id = 28 };
+            Employee employee4 = new Employee() { FirstName = "Ben", LastName = "Solo", Id = 28 };
+
+            // Calls the object method that prints the "FirstName" and "LastName" properties to screen
+            employee1.SayName();
+            employee2.SayName();
+            employee4.SayName();
+            employee3.SayName();
+
+            // This if/else statement compares the Employee objects to see if they are equal in value.  Thanks to the "==" overloading, no additional paramters other than the object name as opposed to the object name and "Id" property are passed to the method.  Text is printed depending on if the "Id" values are the same or different.
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("These employees have the same ID number.");
+            }
+            else
+            {
+                Console.WriteLine("These employees have different ID numbers.");
+            }
+
+            if (employee3 == employee4)
+            {
+                Console.WriteLine("These employees have the same ID number.");
+            }
+            else
+            {
+                Console.WriteLine("These employees have different ID numbers.");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
