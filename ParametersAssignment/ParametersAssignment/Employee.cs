2798212b44783fb49deddf2e7f1a80f15771202b
konadan808 <88794280+konadan808@users.noreply.github.com>
2021-11-25 11:15:51 -0800

    Commit Parameters Assignment
    Committing app created per step 317 of the C# course.  This app demonstrates the use of generic classes and generic lists with the creation of objects and lists based off of the generic class

---
+++ ParametersAssignment/ParametersAssignment/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    public class Employee<T> // This class has been set to generic using "<T>"  This allows an object of any data type to be instantiated using Employee
+    {
+        public List<T> Things { get; set; } // This allows an instantiated object to create a property list in which values can be any data type
+    }
+}
---
+++ ParametersAssignment/ParametersAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> emp1 = new Employee<string>();
+            emp1.Things = new List<string>() { "ID Badge", "Work Computer", "Company Coffee Mug", "Stationary Supplies", "Assigned Parking Spot" };
+
+            Employee<int> emp2 = new Employee<int>();
+            emp2.Things = new List<int>() { 18, 22, 42, 118, 56, 31, 85};
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(emp1.Things[i]);
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine(emp2.Things[i]);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
