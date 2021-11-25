fd8677c8d7d53db366353c3185a7fcf327b3850c
konadan808 <88794280+konadan808@users.noreply.github.com>
2021-11-25 13:26:04 -0800

    Commit of Struct Assignment
    Committing app created per step 331 of the C# course.  This app demonstrates the creation of a struct with a decimal property, and the instantiation of that struct, the assignment of a value to the object, and the value printed to screen

---
+++ StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    // This stuct was created with a decimal data type property called "Amount"
+    public struct Number
+    {
+        public decimal Amount { set; get; }
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiated a new "Number" object called "num1" and set a value to its "Amount" property
+            Number num1 = new Number();
+            num1.Amount = 3.50m;
+            // Instantiated a new "Number" object called "num2" using "num1" as a basis and set the value for it
+            Number num2 = num1;
+            num2.Amount = 19.99m;
+            
+            // Prints the value of both "num1" and "num2" to screen.
+            Console.WriteLine(num1.Amount);
+            Console.WriteLine(num2.Amount);
+
+            Console.ReadLine();
+        }
+    }
+}
