2b782a4b2a6b0d96532a1897e52de72186fb8b7c
konadan808 <88794280+konadan808@users.noreply.github.com>
2021-11-28 11:01:10 -0800

    Commit Input Assignment
    Committing the app created for the Input Assignment per step 365 of the C# course.  This app demonstrates the ability to use the System.IO to write a file, read a file, and print it to screen.  Relative paths were used for the file write and read.

---
+++ InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This code prompts the user to enter a number and sets their input to the string variable "userNum"
+            Console.WriteLine("Hello, plese enter a number.");
+            string userNum = Console.ReadLine();
+            
+            // This line of code uses a relative path method in which it goes back three directories, then into the "log" directory and writes a txt file that contains the user input
+            File.WriteAllText(@"..\..\..\log\userNum.txt", userNum);
+
+            // This line of code uses that same relative path to read the "userNum.txt" file we just created and the "Console.WriteLine()" prints that file to the console
+            Console.WriteLine("Your number is " + File.ReadAllText(@"..\..\..\log\userNum.txt"));
+
+            Console.ReadLine();
+        }
+    }
+}
